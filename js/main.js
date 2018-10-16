669dba20293c3067b2bf1e4be829aec1132bebec
Kyle Hudson <14058890+kylemhudson@users.noreply.github.com>
2018-10-16 05:30:59 -0400

    Got question randomizaton working.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $( document ).ready(function() {
 
+// datastore.js holds all of the following:
 // questionStore = Array of Objects, each holding the questions in order by index.
 // userAnswerStore = Array where we can store the # of the answers that were picked
 // rightAnswerStore = Numbers of the correct answers
+// defaultGif = Gif showing that we're waiting around for you to answer.
+// finalGif = Your suprise for ending the game.
 // wrongReactionGifs = Gifs for wrong answers.
 // rightReactionGifs = Gifs for right answers.
 
@@ -11,11 +14,10 @@ let choiceOne = "";
 let choiceTwo = "";
 let choiceThree = "";
 let choiceFour = "";
+let questionArray = [];
 
 let currentStateStore = userAnswerStore.length;
 let currentQuestionNumber = userAnswerStore.length + 1;
-
-
 
 function displayGameResults() {
 
@@ -132,11 +134,28 @@ function updateNumberCounter() {
 
 function loadQuestions() {
 
-    question = questionStore[currentStateStore].question;
-    choiceOne = questionStore[currentStateStore].answer;
-    choiceTwo = questionStore[currentStateStore].incorrect1;
-    choiceThree = questionStore[currentStateStore].incorrect2;
-    choiceFour = questionStore[currentStateStore].incorrect3;
+  question = questionStore[currentStateStore].question;
+  choiceOne = questionStore[currentStateStore].answer;
+  choiceTwo = questionStore[currentStateStore].incorrect1;
+  choiceThree = questionStore[currentStateStore].incorrect2;
+  choiceFour = questionStore[currentStateStore].incorrect3;
+
+  questionArray.push(choiceOne, choiceTwo, choiceThree, choiceFour);
+  console.log(questionArray);
+  questionRandomize(questionArray);
+
+  function questionRandomize(array) {
+
+      let randomArray = array;
+      randomArray.sort(function() { return 0.5 - Math.random() });
+      console.log(`New shuffled is: ${questionArray}`);
+      }
+
+
+  choiceOne = questionArray[0];
+  choiceTwo = questionArray[1];
+  choiceThree = questionArray[2];
+  choiceFour = questionArray[3];
 
 }
 
@@ -182,15 +201,18 @@ $('.js-next-button').on('click', event => {
 
     if (userAnswerStore.length === 10) {
       alert('OMG. You made it all the way through. Lets tally up some scores');
+      $("img[class='giphy-embed']").attr('src', finalGif[0]);
       displayGameResults();
     } else {
 
     currentStateStore = userAnswerStore.length;
+    questionArray = [];
     loadQuestions();
     updateNumberCounter();
     pushNextQuestion();
     $('.js-responses-container').removeClass('js-right-answer js-wrong-answer');
     $("input[type=radio]").attr('disabled', false);
+    $("img[class='giphy-embed']").attr('src', defaultGif[0]);
     $("form[class='quiz-answer-form']").trigger('reset');
     $("button[class='js-next-button']").toggle();
     $("button[class='js-submit-button']").toggle();
@@ -198,6 +220,5 @@ $('.js-next-button').on('click', event => {
 });
 
 $(handleStartGame());
-
 
 });
